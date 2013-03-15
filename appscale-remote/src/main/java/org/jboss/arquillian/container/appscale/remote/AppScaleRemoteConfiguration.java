f2866993fd9d0b6d3aec276d421872afda9174a3
Matej Lazar <matejonnet@gmail.com>
2013-03-15 15:44:23 +0100

    AppScale container.

---
+++ appscale-remote/src/main/java/org/jboss/arquillian/container/appscale/remote/AppScaleRemoteConfiguration.java
@@ -0,0 +1,70 @@
+/*
+* JBoss, Home of Professional Open Source
+* Copyright $today.year Red Hat Inc. and/or its affiliates and other
+* contributors as indicated by the @author tags. All rights reserved.
+* See the copyright.txt in the distribution for a full listing of
+* individual contributors.
+*
+* This is free software; you can redistribute it and/or modify it
+* under the terms of the GNU Lesser General Public License as
+* published by the Free Software Foundation; either version 2.1 of
+* the License, or (at your option) any later version.
+*
+* This software is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this software; if not, write to the Free
+* Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+* 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+*/
+
+package org.jboss.arquillian.container.appscale.remote;
+
+import org.jboss.arquillian.container.spi.ConfigurationException;
+import org.jboss.arquillian.container.spi.client.container.ContainerConfiguration;
+import org.jboss.arquillian.container.spi.client.deployment.Validate;
+
+/**
+ * AppScale configuration.
+ *
+ * @author <a href="mailto:mlazar@redhat.com">Matej Lazar</a>
+ */
+public class AppScaleRemoteConfiguration implements ContainerConfiguration {
+
+    /**
+     * The e-mail address to use as the app's admin.
+     */
+    private String email;
+
+    /**
+     * Host runing AppScale.
+     */
+    private String remoteHost;
+
+    @Override
+    public void validate() throws ConfigurationException {
+        Validate.notNullOrEmpty(email, "The e-mail address to use as the app's admin must be specified.");
+        Validate.notNullOrEmpty(remoteHost, "Host running AppScale must be specified.");
+
+    }
+
+    public String getRemoteHost() {
+        return remoteHost;
+    }
+
+    public void setRemoteHost(String remoteHost) {
+        this.remoteHost = remoteHost;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
---
+++ appscale-remote/src/test/java/org/jboss/arquillian/container/appscale/remote/AppScaleRemoteClientTestCase.java
@@ -0,0 +1,89 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2010, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.arquillian.container.appscale.remote;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
+ * @author <a href="mailto:mlazar@redhat.com">Matej Lazar</a>
+ */
+@RunWith(Arquillian.class)
+@RunAsClient
+public class AppScaleRemoteClientTestCase {
+
+    /**
+     * Deployment for the test
+     *
+     * @return web archive
+     */
+    @Deployment(name = "default")
+    public static WebArchive getTestArchive() {
+        return ShrinkWrap.create(WebArchive.class, "simple.war")
+                .addClass(TestServlet.class)
+                .setWebXML("gae-web.xml")
+                .addAsWebInfResource("appengine-web.xml")
+                .addAsWebInfResource("logging.properties");
+    }
+
+    @ArquillianResource
+    URL url;
+
+    @Test
+    public void shouldBeAbleToInvokeServletInDeployedWebApp() throws Exception {
+        String body = readAllAndClose(new URL(url.toString() + "test").openStream());
+
+        Assert.assertEquals(
+                "Verify that the servlet was deployed and returns expected result",
+                TestServlet.MESSAGE,
+                body);
+    }
+
+    private String readAllAndClose(InputStream is) throws Exception {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try {
+            int read;
+            while ((read = is.read()) != -1) {
+                out.write(read);
+            }
+        } finally {
+            try {
+                is.close();
+            } catch (Exception ignored) {
+            }
+        }
+        return out.toString();
+    }
+}
